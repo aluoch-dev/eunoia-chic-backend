d2197f50eae217603e936899793ef6c3397600c7
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-07-25 18:17:41 +0300

    add a like api to update post using tentatvive postInfo db represesntation

--- src/server.js
+++ src/server.js
@@ -1,15 +1,34 @@
 import express from "express";
 
+let postsInfo = [
+    {
+        name:'ownership-001',
+        likes: 0
+    },
+    {
+        name: 'success-001',
+        likes: 0
+    },
+    {
+        name: 'birthday-001',
+        likes: 0
+    }
+]
+
 const app = express();
 app.use(express.json());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
-})
+app.put('/api/posts/:name/like', (req, res) => {
+    const { name } = req.params;
+    const post = postsInfo.find(p => p.name === name);
+    if(post) {
+        post.likes += 1
+        res.send(`The ${name} post now has ${ post.likes} likes.`)
+    } else {
+        res.send(`The post ${name} does not exist!`)
+    }
+});
 
-app.post('/hello-name', (req, res) => {
-    res.send(`Hello ${req.body.name}!`);
-})
 
 app.listen(8000, () => {
     console.log('Server listening on port 8000');
