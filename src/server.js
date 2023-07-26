f76ffa9658b20c2dcd63fbd8ed8b93b6a3c3e137
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-07-26 14:51:49 +0300

    update server to use cors and express.json

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 import express from "express";
+import cors from "cors";
 import { db, connectToDb }  from './db.js'
-
-const express = require('express');
-const cors = require('cors');
 
 const app = express();
 app.use(cors(), express.json());
