52da181001849717fd8a339a24d6764197c57b9d
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-07-26 13:08:39 +0300

    update commnets endpoint to use mongodb

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { MongoClient } from "mongodb";
 const app = express();
 app.use(express.json());
 
-app.get('/api/posts/:name', async (req, res) => {
+app.get('/api/posts/:name', async(req, res) => {
     const { name } = req.params;
 
     const client = new MongoClient('mongodb://127.0.0.1:27017');
@@ -22,7 +22,7 @@ app.get('/api/posts/:name', async (req, res) => {
 });
 
 
-app.put('/api/posts/:name/like', async (req, res) => {
+app.put('/api/posts/:name/like', async(req, res) => {
     const { name } = req.params;
 
     const client = new MongoClient("mongodb://127.0.0.1:27017");
@@ -42,14 +42,22 @@ app.put('/api/posts/:name/like', async (req, res) => {
     } 
 });
 
-app.post('/api/posts/:name/comments', (req, res) => {
+app.post('/api/posts/:name/comments', async(req, res) => {
     const { name } = req.params;
     const { postedBy, commentText } = req.body;
 
-    const post = postsInfo.find(p => p.name === name)
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+
+    const db = client.db('eunoia-chic-db')
+    await db.collection('posts').updateOne({ name }, {
+        $push: { comments: { postedBy, commentText }}
+    });
+
+    const post = await db.collection('posts').findOne({ name });
+
 
     if(post) {
-        post.comments.push( { postedBy, commentText })
         res.send(post.comments);
     } else {
         res.send(`The post ${name} does not exist!`)
